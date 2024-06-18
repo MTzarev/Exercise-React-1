56e83beead84c54a316c9528b0705fd566f8810d
Miroslav Tsarev <mtzarev@gmail.com>
2024-06-18 23:21:19 +0300

    Add files via upload

---
+++ InputTest.js
@@ -0,0 +1,34 @@
+function InputTest (){
+
+    return (
+        <li>
+        <input type="text" />
+        <br />
+        <input type="password" />
+        <br />
+        <input type="number" />
+        <br />
+        <input type="checkbox" />
+        <br />
+        <input type="color" />
+        <br />
+        <input type="date" />
+        <br />
+        <input type="time" />
+        <br />
+        <input type="search" /> 
+        <br />
+        <input type="file" />
+        <br />
+        <input type="range" />
+        <br />
+        <input type="radio" />
+        <br />
+        <input type="submit" />
+        <br />
+        <input type="week" />
+        </li>
+    )
+    
+}
+export default InputTest
